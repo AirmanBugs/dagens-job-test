9169158ea1a532e4258e6e72f059f9b38c20fecc
Marius Bang <mariusmbang@gmail.com>
2024-02-22 19:12:00 +0100

    Add form for adding new products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,55 @@
-import React from 'react'
-import {v4 as uuid} from 'uuid'
+import React, { useState } from 'react'
+import {v4 as uuid, validate} from 'uuid'
+import { Product } from '../../interface/types';
 
 export const baseURL = 'http://localhost:3001'
 
 const App: React.FC = () => {
-  const product = {
-    id: uuid(),
-    name: 'pineapple',
-    category: 'greens',
-    price: 42
-  }
+  const [product, setProduct] = useState<Partial<Product>>({})
+
   return (
     <div className="app">
       <h3>Happy hacking!</h3>
-      <button onClick={() => addProduct(product)}>Push me, why don't ya!</button>
+      <h2>Add New Product</h2>
+      <form onSubmit={(event) => {
+          event.preventDefault()
+          const validProduct = validateProduct(product)
+          validProduct && addProduct(validProduct)
+        }}>
+        <section>
+          <label>
+          Name:
+          <input
+            type='text'
+            name='name'
+            onChange={(event) => setProduct({...product, name: event.target.value})}
+          />
+        </label>
+        <label>
+          Category:
+          <input
+            type='text'
+            name='category'
+            onChange={(event) => setProduct({...product, category: event.target.value})}
+          />
+        </label>
+        <label>
+          Price:
+          <input
+            type='text'
+            name='price'
+            onChange={(event) => {
+              const floatValue = parseFloat(event.target.value)
+              setProduct({...product, price: floatValue})
+            }}
+          />
+        </label>
+        </section>
+        <button
+          type='submit'
+          disabled={validateProduct(product) === null}
+        >Add Product</button>
+      </form>
     </div>
   );
 };
@@ -30,5 +66,16 @@ const addProduct = async (product: Product) => {
   const response = await fetch(`${baseURL}/products`, req);
   console.log('Received', response)
 }
+
+const validateProduct = (product: Partial<Product>): Product | null => 
+product.name &&
+product.category &&
+product.price &&
+!Number.isNaN(product.price)  ? {
+  id: uuid(),
+  name: product.name,
+  category: product.category,
+  price: product.price
+} : null
 
 export default App;
